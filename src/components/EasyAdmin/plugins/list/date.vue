03bc5621cc61d86a342807c8fbf8df9e4bd5e1f1
Lam K. <immane@users.noreply.github.com>
2026-07-12 12:54:39 +0800

    feat: extract list field renderers into dynamic plugins (boolean, date, datetime, relation, image, array, plain-text)

---
+++ src/components/EasyAdmin/plugins/list/date.vue
@@ -0,0 +1,18 @@
+<template>
+  <span v-if="value">
+    <i class="el-icon-time" />
+    {{ new Date(value) | dateFormat('YYYY-MM-DD') }}
+  </span>
+</template>
+
+<script>
+export default {
+  props: {
+    value: { type: [String, Number, Boolean, Object, Array], default: null },
+    field: { type: Object, default: () => ({}) },
+    scope: { type: Object, default: () => ({}) },
+    em: { type: Object, default: () => ({}) },
+    struct: { type: Object, default: () => ({}) }
+  }
+}
+</script>
