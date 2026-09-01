4b403f44155a32354f4793ccf4fe862ea6e56c95
Lam K. <immane@users.noreply.github.com>
2026-09-01 20:23:39 +0800

    style(transfer): tighten spacing and wrap checkbox labels
    - Use CSS vars to compact panel (280px/240px) and avoid 400px gap
    - Allow checkbox labels to wrap (normal/visible/line-height 1.4) and center checkbox vertically

--- src/components/EasyAdmin/plugins/form/transfer.vue
+++ src/components/EasyAdmin/plugins/form/transfer.vue
@@ -48,12 +48,36 @@ export default {
 
 <style lang="scss">
 .transfer-field {
+  --el-transfer-panel-width: 280px;
+  --el-transfer-panel-body-height: 240px;
+
   .el-transfer-panel {
-      width: 300px;
-      height: 400px;
+      width: var(--el-transfer-panel-width);
+      height: auto;
   }
   .el-transfer-panel__list.is-filterable {
-      height: 290px;
+      height: calc(100% - var(--el-transfer-filter-height) - 30px);
+  }
+  .el-transfer__buttons {
+    padding: 0 12px;
+  }
+  .el-transfer-panel__item.el-checkbox {
+    position: relative;
+    height: auto;
+    min-height: 32px;
+    padding-top: 6px;
+    padding-bottom: 6px;
+  }
+  .el-transfer-panel__item.el-checkbox .el-checkbox__input {
+    top: 50%;
+    transform: translateY(-50%);
+  }
+  .el-transfer-panel__item.el-checkbox .el-checkbox__label {
+    white-space: normal;
+    text-overflow: clip;
+    overflow: visible;
+    min-height: 20px;
+    line-height: 1.4;
   }
 }
 </style>
